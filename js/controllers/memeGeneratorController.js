804e60a0f13558488925abdefbf60717f35df157
Nir Sultan <snufkin62@gmail.com>
2025-04-02 12:26:24 +0300

    Basic render meme and text

--- js/controllers/memeGeneratorController.js
+++ js/controllers/memeGeneratorController.js
@@ -1,2 +1,32 @@
 'use strict'
 
+let gElCanvas
+let gCtx
+
+function onInit() {
+  gElCanvas = document.querySelector('canvas')
+  gCtx = gElCanvas.getContext('2d')
+
+  resizeCanvas()
+  window.addEventListener('resize', resizeCanvas)
+}
+
+function resizeCanvas() {
+  const elContainer = document.querySelector('.canvas-container')
+  gElCanvas.width = elContainer.clientWidth - 40
+}
+
+function renderMeme(imgId, txt = 'Enter txt') {
+  const img = new Image()
+  img.src = getImgById(imgId).url
+  img.onload = function () {
+    gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+    renderTxt(txt)
+  }
+}
+
+function renderTxt(txt = 'Enter text') {
+  gCtx.font = "25px arial"
+  gCtx.fillStyle = 'white'
+  gCtx.fillText(txt, 20, 20, gElCanvas.width - 20)
+}
--- js/services/memeGeneratorService.js
+++ js/services/memeGeneratorService.js
@@ -1,1 +1,25 @@
 'use strict'
+
+var gImgs = [
+  { id: 1, url: 'img/1.jpg', keywords: ['politics', 'trump', 'president'] },
+  { id: 2, url: 'img/2.jpg', keywords: ['puppy', 'funny', 'animals'] },
+  { id: 3, url: 'img/3.jpg', keywords: ['puppy', 'funny', 'animals', 'baby', 'sleep'] },
+]
+
+var gMeme = {
+  selectedImgId: 5,
+  selectedLineIdx: 0,
+  lines: [
+    {
+      txt: 'I sometimes eat Falafel',
+      size: 20,
+      color: 'red'
+    }
+  ]
+}
+
+var gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
+
+function getImgById(imgId) {
+  return gImgs.find(img => img.id === imgId)
+}
