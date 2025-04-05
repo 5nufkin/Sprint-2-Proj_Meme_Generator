02c44e28b123bddce174d05a6c91aa3a449dbf6b
Nir Sultan <snufkin62@gmail.com>
2025-04-05 21:15:06 +0300

    Support various aspect ratios

--- js/services/memeGeneratorService.js
+++ js/services/memeGeneratorService.js
@@ -19,6 +19,13 @@ var gImgs = [
   { id: 16, url: 'img/16.jpg', keywords: ['movie', 'funny', 'viral', 'laugh', 'actor'] },
   { id: 17, url: 'img/17.jpg', keywords: ['putin', 'funny', 'russia', 'president', 'politics'] },
   { id: 18, url: 'img/18.jpg', keywords: ['movie', 'funny', 'cartoon', 'buzz', 'woody', 'toys'] },
+  { id: 19, url: 'img/19.jpg', keywords: ['movie', 'funny', 'cartoon', 'mean', 'sad', 'angry'] },
+  { id: 20, url: 'img/20.jpg', keywords: ['israeli', 'shahar', 'viral', 'angry', 'scream', 'what', 'old'] },
+  { id: 21, url: 'img/21.jpg', keywords: ['oprah', 'winfrey', 'success', 'famous', 'funny', 'scream'] },
+  { id: 22, url: 'img/22.jpg', keywords: ['politics', 'trump', 'president', 'angry', 'pointing'] },
+  { id: 23, url: 'img/23.jpg', keywords: ['dress', 'dance', 'mountain', 'girl', 'happy', 'smile', 'nature'] },
+  { id: 24, url: 'img/24.jpg', keywords: ['funny', 'dance', 'africa', 'kids', 'happy', 'smile',] },
+  { id: 25, url: 'img/25.jpg', keywords: ['dog', 'weird', 'animals', 'nature'] },
 ]
 
 var gMeme = {
